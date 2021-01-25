79879f983be03ddc72e90d4936c85bb74a78e665
Anish Tamang <anish.tmngo@gmail.com>
2021-01-25 17:46:23 -0500

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,13 +6,13 @@ from devbops_user_microservice import app
 
 class BasicTestCase(unittest.TestCase):
 
-#     def setUp(self):
-#         unittest.TestLoader.sortTestMethodsUsing = None
-#         app.config['TESTING'] = True
-#         app.config['WTF_CSRF_ENABLED'] = False
-#         app.config['DEBUG'] = False
-#         self.app = app.test_client()
-#         self.assertEqual(app.debug, False)
+    def setUp(self):
+        unittest.TestLoader.sortTestMethodsUsing = None
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEqual(app.debug, False)
 
     def test_1_signup(self):
         req = {
